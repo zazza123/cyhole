019ba2fb6ccc3e6a5e37e28b4918dcb3947273b1
zazza.py <andrea.za94@gmail.com>
2024-08-22 11:36:23 +0200

    ADD: introduced new api_return_model function in Interaction class

--- src/cyhole/core/interaction.py
+++ src/cyhole/core/interaction.py
@@ -1,6 +1,9 @@
-from typing import Any
+from typing import Any, TypeVar, Type, Coroutine
+from pydantic import BaseModel
 
 from ..core.client import APIClient, AsyncAPIClient
+
+ResponseModel = TypeVar('ResponseModel', bound = BaseModel)
 
 class Interaction:
     """
@@ -26,3 +29,34 @@ class Interaction:
         self.async_client = AsyncAPIClient(self, headers = self.headers)
 
         return
+
+    def api_return_model(self, sync: bool, type: str, url: str, response_model: Type[ResponseModel], *args: tuple, **kwargs: dict[str, Any]) -> ResponseModel | Coroutine[None, None, ResponseModel]:
+        """
+            This function is used to execute a request to the API by forwarding the call to the 
+            corresponding client (synchronous or asynchronous) according to the `sync` parameter.
+
+            This function is used to avoid code duplication in the methods of the `Interaction` classes, 
+            and it assumes that the response of the API request is a JSON object that can be parsed into 
+            a `pydantic.BaseModel` model.
+
+            Parameters:
+                sync: boolean to define if the request is synchronous or asynchronous.
+                type: request's type ([`RequestType`][cyhole.core.param.RequestType]).
+                url: the URL of the API endpoint.
+                response_model: the `pydantic.BaseModel` model used to parse the response.
+                args: the positional arguments to be passed to the API request.
+                kwargs: the named arguments to be passed to the API request.
+
+            Returns:
+                The response of the API request. If the request is synchronous, the response is a 
+                `pydantic.BaseModel` model. If the request is asynchronous, the response is a coroutine 
+                that will return a `pydantic.BaseModel` model.
+        """
+        if sync:
+            content_raw = self.client.api(type, url, *args, **kwargs)
+            return response_model(**content_raw.json())
+        else:
+            async def async_request():
+                content_raw = await self.async_client.api(type, url, *args, **kwargs)
+                return response_model(**content_raw.json())
+            return async_request()
