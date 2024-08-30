8395acbf9f8c3cc10fd3f0fc656cb1e6500a08a2
zazza.py <andrea.za94@gmail.com>
2024-08-30 23:10:46 +0200

    ADD: management of V1 and V2 endpoints

--- src/cyhole/solscan/client.py
+++ src/cyhole/solscan/client.py
@@ -1,10 +1,28 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, cast
 
 from ..core.client import APIClient, AsyncAPIClient
 
 if TYPE_CHECKING:
     from ..solscan.interaction import Solscan
+
+class SolscanV1Client(APIClient):
+    """
+        Client used for synchronous API calls for `Solscan` interaction on **V1** API.
+    """
+
+    def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
+        super().__init__(interaction, headers)
+        self._interaction = cast(Solscan, self._interaction)
+
+class SolscanV2Client(APIClient):
+    """
+        Client used for synchronous API calls for `Solscan` interaction on **V2** API.
+    """
+
+    def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
+        super().__init__(interaction, headers)
+        self._interaction = cast(Solscan, self._interaction)
 
 class SolscanClient(APIClient):
     """
@@ -13,7 +31,29 @@ class SolscanClient(APIClient):
 
     def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
         super().__init__(interaction, headers)
-        self._interaction: Solscan = self._interaction
+        self._interaction = cast(Solscan, self._interaction)
+
+        # set API versions clients
+        self.v1 = SolscanV1Client(self._interaction, headers = {"token": self._interaction.api_key_v1})
+        self.v2 = SolscanV2Client(self._interaction, headers = {"token": self._interaction.api_key_v2})
+
+class SolscanV1AsyncClient(AsyncAPIClient):
+    """
+        Client used for asynchronous API calls for `Solscan` interaction on **V1** API.
+    """
+
+    def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
+        super().__init__(interaction, headers)
+        self._interaction = cast(Solscan, self._interaction)
+
+class SolscanV2AsyncClient(AsyncAPIClient):
+    """
+        Client used for asynchronous API calls for `Solscan` interaction on **V2** API.
+    """
+
+    def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
+        super().__init__(interaction, headers)
+        self._interaction = cast(Solscan, self._interaction)
 
 class SolscanAsyncClient(AsyncAPIClient):
     """
@@ -22,4 +62,8 @@ class SolscanAsyncClient(AsyncAPIClient):
 
     def __init__(self, interaction: Solscan, headers: Any | None = None) -> None:
         super().__init__(interaction, headers)
-        self._interaction: Solscan = self._interaction
+        self._interaction = cast(Solscan, self._interaction)
+
+        # set API versions clients
+        self.v1 = SolscanV1AsyncClient(self._interaction, headers = {"token": self._interaction.api_key_v1})
+        self.v2 = SolscanV2AsyncClient(self._interaction, headers = {"token": self._interaction.api_key_v2})
--- src/cyhole/solscan/interaction.py
+++ src/cyhole/solscan/interaction.py
@@ -1,3 +1,4 @@
+import os
 from requests.exceptions import HTTPError
 from typing import Any, Coroutine, overload, Literal
 
@@ -18,11 +19,28 @@ class Solscan(Interaction):
         - **v2** - a new and improved version of the Pro API, with more endpoints and features.
             This version is under development and may have some changes.
 
+        Each client (`sync` and `async`) has two attributes `v1` and `v2` that represent the API 
+        versions; in this way, you can access the endpoints of each version separately. 
+
+        !!! info
+            Both versions require different API keys for access. If the API keys are not 
+            provided during the object creation, then they are automatically retrieved from 
+            environment variables `SOLSCAN_API_V1_KEY` and `SOLSCAN_API_V2_KEY` respectively.
+
+        Parameters:
+            api_key_v1: specifies the API key for Solscan Pro API v1.
+            api_key_v2: specifies the API key for Solscan Pro API v2.
+
         **Example**
     """
 
-    def __init__(self, headers: Any | None = None) -> None:
-        super().__init__(headers)
+    def __init__(self, api_key_v1: str | None = None, api_key_v2: str | None = None) -> None:
+
+        # set APIs
+        self.api_key_v1 = api_key_v1 if api_key_v1 is not None else os.environ.get("SOLSCAN_API_V1_KEY")
+        self.api_key_v2 = api_key_v2 if api_key_v2 is not None else os.environ.get("SOLSCAN_API_V2_KEY")
+
+        super().__init__()
 
         # clients
         self.client = SolscanClient(self)
